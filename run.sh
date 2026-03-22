23f3357bd05e33a284415eb41eb2365010bd5f9d
Sena Folikumah <68739642+self-reliantkid@users.noreply.github.com>
2026-03-22 20:04:18 +0000

    Update run.sh to ensure app is executable

--- run.sh
+++ run.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+# Get the directory where this script lives
 DIR="$(cd "$(dirname "$0")" && pwd)"
 APP="$DIR/compound_interest"
 
+# Make sure it's executable
 chmod +x "$APP"
+
+# Run the app
 "$APP"
 
+# Keep Terminal open after exit
 echo ""
 echo "Press any key to close..."
 read -n 1
